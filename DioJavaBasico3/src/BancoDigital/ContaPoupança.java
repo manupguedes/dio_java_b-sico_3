d475c9ab7369591e1aadcf37c0f4ccf0b667228a
Manoela <148467291+manupguedes@users.noreply.github.com>
2024-06-01 17:00:16 -0300

    ContaPoupança.java

--- DioJavaBasico3/src/BancoDigital/ContaPoupança.java
+++ DioJavaBasico3/src/BancoDigital/ContaPoupança.java
@@ -1,6 +1,6 @@
 package BancoDigital;
 public class ContaPoupança extends Conta {
-    @Override
+  
     public void imprimirExtrato() {
         System.out.println("=== Extrato Conta Poupança ===");
         super.imprimirExtrato();
