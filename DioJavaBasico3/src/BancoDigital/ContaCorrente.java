bcf76a1dbd00984ce950642a3b59ead21e1fa7f2
Manoela <148467291+manupguedes@users.noreply.github.com>
2024-06-01 17:00:02 -0300

    ContaCorrente.java

--- DioJavaBasico3/src/BancoDigital/ContaCorrente.java
+++ DioJavaBasico3/src/BancoDigital/ContaCorrente.java
@@ -1,6 +1,6 @@
 package BancoDigital;
 public class ContaCorrente extends Conta {
-    @Override
+  
     public void imprimirExtrato() {
         System.out.println("=== Extrato Conta Corrente ===");
         super.imprimirExtrato();
